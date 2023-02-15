64eaba3884849e801b77d7de39bd7ebf2b2bb81e
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-02-15 19:46:56 +0530

    created README.md file

--- IAM-Role/main.tf
+++ IAM-Role/main.tf
@@ -5,7 +5,10 @@ module "shared_vars" {
 
 resource "aws_iam_role" "media_convert_role" {
   name = "MediaConvertCustomRole"
-
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "VODLambdaRole"
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AWSLambda_FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess"]
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Video Streaming System Design
